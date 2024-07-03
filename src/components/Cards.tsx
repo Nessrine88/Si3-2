bdcdd001fc52919f581a133cffd4df6ed6d0f0b4
Nessrine88 <macherki.nessrine@yahoo.com>
2024-07-03 22:45:55 +0100

    fix mobile layout

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -1,19 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { getCards } from '@/lib/types/cards';
-export const revalidate = 3;
 
-// import {Pagination, PaginationItem, PaginationCursor} from "@nextui-org/pagination";
+const Cards = async() => {
+ 
+      const cards = await getCards();
+   
 
-const Cards = async () => {
-  const cards = await getCards();
   return (
-    <div className="bg-[url('/images/bottombg.png')] bg-no-repeat bg-left-bottom bg-[length:80vw_130vh] ">
-      <div className="md:flex md:justify-center  bg-[url('/images/rightBg.png')] bg-[length:80vw_300vh] bg-no-repeat bg-right-top">
-        <div className="md:grid lg:grid-cols-3 md:grid-cols-2 md:m-[4rem] md:mb-10 gap-[30px] ">
+    <div className="md:bg-[url('/images/bottombg.png')] md:bg-no-repeat md:bg-left-bottom md:bg-[length:80vw_130vh]">
+      <div className="md:flex md:justify-center md:bg-[url('/images/rightBg.png')] md:bg-[length:80vw_300vh] md:bg-no-repeat md:bg-right-top">
+        <div className="md:grid lg:grid-cols-3 md:grid-cols-2 md:m-[4rem] md:mb-10 gap-[30px]">
           {cards.map((card, index) => (
-            <div key={index} className="relative p-[25px]  w-[412px] h-[549px] h-max-fit border-[2px] border-[#FAB7D0] rounded-xl">
+            <div key={index} className="relative p-[25px] w-full md:w-[412px] h-[549px] md:h-max-fit border-[2px] border-[#FAB7D0] rounded-xl">
               <div className="flex">
-                <div className="shadaw iconCard relative bg-gradient-to-b from-[#3E21F333] to-[#A020F0] p-[1.15px]  w-[87px] h-[87px] rounded-md">
+                <div className="shadaw iconCard relative bg-gradient-to-b from-[#3E21F333] to-[#A020F0] p-[1.15px] w-[87px] h-[87px] rounded-md">
                   <img
                     src={card.cardIcon.asset.url}
                     alt=""
@@ -21,40 +21,37 @@ const Cards = async () => {
                   />
                 </div>
                 <div className="ml-4">
-                  <p className="clash uppercase font-medium text-[#404040] w-[259px] text-[30px] leading-[36px] ">{card.title}</p>
-                  <p className="text-[16px] leading-[25px]  bg-[#A2FF9324] bg-opacity-[14%] rounded-[10px] mt-3 px-4  text-center w-fit roboto-mono">{card.status}</p>
+                  <p className="clash uppercase font-medium text-[#404040] md:w-[259px] text-[30px] leading-[36px]">{card.title}</p>
+                  <p className="text-[16px] leading-[25px] bg-[#A2FF9324] bg-opacity-[14%] rounded-[10px] mt-3 px-4 text-center w-fit roboto-mono">{card.status}</p>
                 </div>
               </div>
               <div>
-                <p className="text-[#696969] text-[16px] leading-[20px] fira-mono-regular pt-5  h-[114px]">
+                <p className="text-[#696969] text-[16px] leading-[20px] fira-mono-regular pt-5 h-[114px]">
                   {card.description}
                 </p>
               </div>
-              <div className=" mt-32 max-h-36 flex flex-col ">
+              <div className="mt-32 md:max-h-36 flex flex-col">
                 {card.links.map((link, linkIndex) => (
-                  <div key={linkIndex} className="flex items-center  ">
-                    <div className="w-[20px] h-[20px] mr-2 ">
+                  <div key={linkIndex} className="flex items-center">
+                    <div className="w-[20px] h-[20px] mr-2">
                       <img className="w-full h-full object-cover" src={link.icon.asset.url} alt="" />
                     </div>
-                    <p className="text-[#4428F2] leading-[30px] font-medium -tracking-normal text-[16px] clash">{link.name}</p>
+                    <p className="text-[#4428F2] leading-[30px] font-medium tracking-normal text-[16px] clash">{link.name}</p>
                   </div>
                 ))}
-                <div className='absolute w-[85%] bottom-0 py-5 mx-auto'>
-                <div className="p-[1px]">
-                  <button className="z-10 clash font-medium text-[20px] leading-[30px] border-gradient text-center py-[8px] custom-border-gradient w-full rounded-lg">
-                    App List’s Common Ground
-                  </button>
-                </div>
+                <div className="absolute w-full bottom-0 py-5 mx-auto">
+                  <div className="p-[1px]">
+                    <button className="z-10 clash font-medium text-[20px] leading-[30px] border-gradient text-center py-[8px] custom-border-gradient w-full rounded-lg">
+                      App List’s Common Ground
+                    </button>
+                  </div>
                 </div>
               </div>
-
-              
             </div>
           ))}
         </div>
       </div>
       <div className="flex gap-7 justify-center pb-32 mx-20">
-      {/* <Pagination showControls loop  total={3} initialPage={1} style={{borderColor:"#FAB7D0"}} /> */}
         <div className="w-12 h-12 rounded-md border-[#FAB7D0] border flex justify-center items-center hover:bg-black hover:text-white"><i className="fas fa-chevron-left text-sm"></i></div>
         <div className="w-12 h-12 rounded-md border-[#FAB7D0] border flex justify-center items-center hover:bg-black hover:text-white">1</div>
         <div className="w-12 h-12 rounded-md border-[#FAB7D0] border flex justify-center items-center hover:bg-black hover:text-white">2</div>
--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,19 +1,19 @@
 "use client"
-import React from 'react';
+import React, { useState } from 'react';
 import CardPopup from './CardPopup';
-import { useState } from 'react';
-import "../app/globals.css"
+import "../app/globals.css";
 
-const HomePage = () => {
+const HomePage = ({ setSearchQuery }: any) => {
   const [showPopup, setShowPopup] = useState(false);
 
   const togglePopup = () => {
     setShowPopup(!showPopup);
   };
+
   return (
-    <div className=" md:pt-[170px] pt-[9rem] min-h-screen w-full md:p-[4rem] p-[2rem] bg-[url('/images/bgGradient.png')] bg-no-repeat bg-[#d6b3d2] ">
+    <div className="md:pt-[170px] pt-[9rem] min-h-screen min-w-full md:p-[4rem] p-[2rem] bg-[url('/images/bgGradient.png')] bg-no-repeat bg-[#d6b3d2]">
       <div className="flex flex-col lg:flex-row justify-between">
-        <div className="font-semibold text-[32px] lg:text-[64px] leading-[40px] lg:leading-[79px] uppercase text-[#4428F2] w-[839px] lg:w-[60vw] clash mb-8 lg:mb-0">
+        <div className="font-semibold text-[32px] lg:text-[64px] leading-[40px] lg:leading-[79px] uppercase text-[#4428F2] clash mb-8 md:w-[56%]">
           <h1 className="text-white">
             Discover The 
           </h1>
@@ -21,52 +21,56 @@ const HomePage = () => {
             WOMEN & NON-BINARY WEB3 ECOSYSTEM.
           </h1>
         </div>
-        <div className="flex h-fit items-center flex-col lg:flex-row mr-28">
-          <div className=" flex items-center mb-4 lg:mb-0  lg:mr-[50px] gap-5">
-            <p className="fira-mono-regular text-white  flex gap-2">
+        <div className="flex h-fit items-center flex-col lg:flex-row mr-6 lg:mr-28">
+          <div className="flex items-center mb-4 lg:mb-0 lg:mr-[50px] gap-5">
+            <p className="fira-mono-regular text-white flex gap-2">
               Info
-            <img
-              src={"/images/exclamation.png"}
-              alt=""
-              width={46}
-              height={60}
-              className="w-5 h-5"
-            />
+              <img
+                src={"/images/exclamation.png"}
+                alt=""
+                width={46}
+                height={60}
+                className="w-5 h-5"
+              />
             </p>
           </div>
-          <button className="fira-mono-regular uppercase rounded-[8px] border border-white text-white px-4 py-2"  onClick={togglePopup} >
+          <button className="fira-mono-regular uppercase rounded-[8px] border border-white text-white px-4 py-2" onClick={togglePopup}>
             Add a community
           </button>
           <CardPopup show={showPopup} handleClose={togglePopup} />
         </div>
       </div>
-      <div className="fira-mono-regular  flex flex-col justify-between lg:flex-row mr-28 mt-8 gap-12">
-        <div className=" leading-6 flex items-center border border-white text-white px-4 py-2 rounded-[8px] w-full lg:w-[42vw] mb-4 lg:mb-0">
-          <div className="w-6 h-6 mr-2 flex items-center ">
-          <img
-            src={'/images/bigSearch.png'}
-            className="mr-3 object-fit w-full h-full"
-          />
+      <div className="flex flex-col lg:flex-row justify-between mr-6 lg:mr-28 mt-8 gap-12">
+        <div className="flex items-center border border-white px-4 py-2 rounded-[8px] w-full lg:w-[42vw] mb-4 lg:mb-0">
+          <div className="w-6 h-6 mr-2 flex items-center">
+            <img
+              src={'/images/bigSearch.png'}
+              className="mr-3 object-fit w-full h-full"
+            />
           </div>
-          <p>Search By Name, Location, Description, Values.</p>
+          <input
+            className="w-full bg-transparent leading-6 placeholder-white fira-mono-regular outline-none cursor-white caret-white text-white"
+            placeholder='Search By Name, Location, Description, Values.'
+            onChange={(e) => setSearchQuery(e.target.value)}
+          />
         </div>
         <div className="flex items-center border border-white text-white px-4 py-2 rounded-[8px] w-full lg:w-[20vw] mb-4 lg:mb-0">
           <p className="mr-3">Community Type</p>
           <img
             src={'/arrow-down.svg'}
-            className='w-[24px] h-[24px] '
+            className='w-[24px] h-[24px]'
           />
         </div>
         <div className="flex items-center border border-white text-white px-4 py-2 rounded-[8px] w-full lg:w-[20vw]">
           <p className="mr-3">Community Location</p>
           <img
             src={'/arrow-down.svg'}
-            className='w-[24px] h-[24px] '
+            className='w-[24px] h-[24px]'
           />
         </div>
       </div>
-      <div className="mt-14 py-6  bg-[#D574B633] bg-[url('/images/rectangle.png')] bg-no-repeat bg-cover bg-center bg-opacity-30 min-h-[30vh] rounded-[8px] flex flex-col justify-center items-center text-center p-6">
-        <p className="clash text-[#4428F2] text-[20px] lg:text-[35px] font-medium leading-[30px] lg:leading-[68px] mb-[14px] ">JOIN OUR COMMON GROUND.</p>
+      <div className="mt-14 py-6 bg-[#D574B633] bg-[url('/images/rectangle.png')] bg-no-repeat bg-cover bg-center bg-opacity-30 min-h-[30vh] rounded-[8px] flex flex-col justify-center items-center text-center p-6">
+        <p className="clash text-[#4428F2] text-[20px] lg:text-[35px] font-medium leading-[30px] lg:leading-[68px] mb-[14px]">JOIN OUR COMMON GROUND.</p>
         <p className="text-[#1C1B22] text-[14px] lg:text-[20px] leading-[20px] lg:leading-[26px] fira-mono-regular mb-10">
           If you want to get tips for UX job searching, subscribe to my UX Jetpack newsletter. <br /> View at https://uxjetpack.com/newsletter
         </p>
