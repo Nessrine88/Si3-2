d34257e0100c859791aa2134feaf3741f0eb44a9
Nessrine88 <macherki.nessrine@yahoo.com>
2024-07-04 09:50:26 +0100

    add mobile version

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -52,21 +52,23 @@ const HomePage = ({ setSearchQuery }: any) => {
   }, []);
 
   return (
-    <div className="pt-36 min-h-screen w-full md:py-16 py-8 px-4 md:px-16 bg-[url('/images/bgGradient.png')] bg-no-repeat bg-[#d6b3d2] overflow-x-hidden box-border">
-      <div className="flex flex-col lg:flex-row justify-between items-start lg:items-center w-full">
-        <div className="font-semibold text-2xl lg:text-6xl leading-10 lg:leading-[79px] uppercase text-[#4428F2] clash mb-8 md:w-[56%]">
+    <div className="pb-10  md:pt-36 pt-24 min-h-screen w-full  px-4 md:px-16 bg-[url('/images/bgGradient.png')] bg-no-repeat bg-[#d6b3d2]">
+      <div className="flex flex-col lg:flex-row justify-between items-start  w-full">
+        <div className="font-semibold md:text-[64px] text-[28px] lg:text-6xl leading-10 md:leading-[79px] uppercase text-[#4428F2] clash mb-8 md:w-[56%]">
           <h1 className="text-white">Discover The</h1>
           <h1>WOMEN & NON-BINARY WEB3 ECOSYSTEM.</h1>
         </div>
-        <div className="flex flex-col lg:flex-row items-center lg:mr-28 w-full lg:w-auto">
-          <div className="flex items-center justify-center mb-4 lg:mb-0 lg:mr-12 gap-5 w-full lg:w-auto">
-            <p className="fira-mono-regular text-[#4428F2] text-lg flex gap-2 items-center">
+        <div className="flex flex-col md:flex-row items-center md:mr-28 w-full md:w-auto">
+          <div className="flex items-center justify-center  mb-4 lg:mb-0 lg:mr-12 gap-5 w-full md:w-auto">
+            <p className="fira-mono-bold md:fira-mono-regular text-[#4428F2] text-lg flex gap-2 items-center ">
               Info
+              <div>
               <i className="far fa-question-circle w-6 h-6"></i>
+              </div>
             </p>
           </div>
           <button
-            className="fira-mono-regular uppercase rounded-lg text-white px-7 py-2 bg-black w-full lg:w-auto"
+            className="text-[14px] clash uppercase rounded-lg text-white px-7 py-2 bg-black w-full lg:w-auto md:text-[20px] leading-[30px] font-medium "
             onClick={togglePopup}
           >
             Add a community
@@ -74,9 +76,9 @@ const HomePage = ({ setSearchQuery }: any) => {
           <CardPopup show={showPopup} handleClose={togglePopup} />
         </div>
       </div>
-      <div className="flex flex-col lg:flex-row justify-between lg:mr-28 mt-8 gap-4 lg:gap-12 w-full">
-        <div className="relative flex items-center border border-white px-4 py-2 rounded-lg w-full lg:w-[42vw]">
-          <div className="w-6 h-6 mr-2 flex items-center">
+      <div className="md:text-[20px] text-[14px] flex flex-col lg:flex-row justify-between lg:mr-28 mt-5 gap-4 lg:gap-12 w-full">
+        <div className=" relative flex items-center border border-white px-4 py-2 rounded-lg w-full ">
+          <div className="md:w-6 md:h-6 w-5 h-5 mr-2 flex items-center">
             <img
               src={'/images/bigSearch.png'}
               className="object-fit w-full h-full"
@@ -95,8 +97,8 @@ const HomePage = ({ setSearchQuery }: any) => {
             onClick={toggleDropDown}
             ref={dropdownRef1}
           >
-            <p className="mr-3">Community Type</p>
-            <img src={'/arrow-down.svg'} className="w-6 h-6" />
+            <p className="md:mr-3 mr-2 ">Community Type</p>
+            <img src={'/arrow-down.svg'} className="md:w-6 md:h-6 w-5 h-" />
             <div
               className={`dropdown ${
                 dropDownOpen ? 'block' : 'hidden'
@@ -116,8 +118,8 @@ const HomePage = ({ setSearchQuery }: any) => {
             onClick={toggleDropDown2}
             ref={dropdownRef2}
           >
-            <p className="mr-3">Community Location</p>
-            <img src={'/arrow-down.svg'} className="w-6 h-6" />
+            <p className="md:mr-3 mr-2">Community Location</p>
+            <img src={'/arrow-down.svg'} className="md:w-6 md:h-6 w-5 h-5" />
             <div
               className={`dropdown ${
                 dropDownOpen2 ? 'block' : 'hidden'
